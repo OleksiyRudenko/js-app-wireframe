fbf698b07601d8ee591e54bc8491fd87a283ebd8
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2017-08-14 19:59:43 +0300

    example: ES6 class

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-const color = require('color');
-const redHexa = color({ r: 255, g: 0, b:0}).hex();
-console.log(redHexa);
+// const Dog = require('./dog')
+import Dog from './dog'
+
+const toby = new Dog('Toby')
+
+console.log(toby.bark())
